49e175fa8871e0f93c2f7199db0da72d916e7a9c
bljr07 <beyblade1704@gmail.com>
2026-01-28 12:27:53 +0800

    FIX: Fixed styling of place order btn

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -287,7 +287,7 @@ const submitOrder = async () => {
             </label>
           </div>
 
-          <button @click="submitOrder" class="btn bg-dark-pink w-100 btn-lg mt-3"
+          <button @click="submitOrder" class="btn bg-dark-pink w-100 btn-lg mt-3 fw-bold" style="color: black; border-color: black;"
             :disabled="isSubmitting || isUploading || cart.length === 0 || !customer.payment_proof || !customer.pdpa_consent">
             {{ isSubmitting ? 'Processing...' : 'Confirm Payment & Order' }}
           </button>
